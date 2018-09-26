1855960798afddd87f8f1348f532a9ff6792c1fb
Naccyde <naccyde@naccyde.eu>
2018-09-26 12:11:20 +0200

    Ass inb assembly function

--- src/io/io.s
+++ src/io/io.s
@@ -1,11 +1,21 @@
-global outb			; Set outb visible out of this file
+; Set symbols visible outside
+global outb
+global inb
 
 ; outb - Send a byte to an I/O port
 ; stack: [esp + 8] data byte
 ;	 [esp + 4] I/O port
-;	 [esp    ] return address
+;	 [esp    ] Return address
 outb:
 	mov al, [esp + 8]
 	mov dx, [esp + 4]
 	out dx, al
+	ret
+
+; inb - Read a byte from an I/O port
+; stack: [esp + 4] I/O port
+;	 [esp    ] Return address
+inb:
+	mov dx, [esp + 4]
+	in al, dx
 	ret
