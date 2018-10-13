ac5721330d005d3a6ecb2172aab263449bf32ab3
naccyde <naccyde@naccyde.eu>
2018-10-13 14:15:03 +0200

    Add strrev and itoa

--- include/nucleus/lib/string.h
+++ include/nucleus/lib/string.h
@@ -2,7 +2,10 @@
 #define _NUCLEUS_LIB_STRING_H
 
 #include <nucleus/types.h>
+#include <nucleus/bool.h>
 
-size_t strlen(const uint8_t *s); 
+size_t strlen(const uint8_t *s);
+void strrev(uint8_t *s);
+bool itoa(int32_t value, uint8_t *s, size_t len);
 
 #endif
--- nucleus/lib/string.c
+++ nucleus/lib/string.c
@@ -9,3 +9,53 @@ size_t strlen(const uint8_t *s)
 
 	return len;
 }
+
+void strrev(uint8_t *s)
+{
+	uint8_t c;
+
+	for (size_t i = 0, j = strlen(s) - 1; i < j; ++i, --j) {
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c; 
+	}
+}
+
+bool itoa(int32_t value, uint8_t *s, size_t len)
+{
+	size_t i = 0;
+	bool ret = true;
+	bool negative = false;
+	
+	if (2 > len) {
+		ret = false;
+		goto end;
+	}
+
+	if (value < 0) {
+		negative = true;
+		value = -value;
+	}
+
+	do {
+		s[i++] = value % 10 + '0';
+		--len;
+	} while ((value /= 10) > 0 && len > 1);
+
+	if (negative) {
+		s[i++] = '-';
+		--len;
+	}
+	
+	if (0 == len) {
+		ret = false;
+		goto end;
+	}
+
+	s[i++] = '\0';
+
+	strrev(s);
+
+end:
+	return ret;
+}
