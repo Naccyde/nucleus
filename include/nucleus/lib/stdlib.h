75c1993db7331be3a979a9ddd6f60633dea5bed4
Naccyde <naccyde@naccyde.eu>
2018-10-19 18:00:10 +0200

    Improve panic screen

---
+++ include/nucleus/lib/stdlib.h
@@ -0,0 +1,8 @@
+#ifndef _NUCLEUS_LIB_STDLIB_H
+#define _NUCLEUS_LIB_STDLIB_H
+
+#include <nucleus/types.h>
+
+#define NULL (void *)0
+
+#endif
--- nucleus/debug.c
+++ nucleus/debug.c
@@ -12,7 +12,19 @@ void log(const uint8_t *s)
 	puts(s);
 }
 
-void panic(void)
+void panic(struct machine_state *state)
 {
-	vga_panic();
+	vga_hide_cursor();
+	
+	vga_clr_from_to(0, 0, VGA_CON_WIDTH, 0, ' ', VGA_COLOR_WHITE,
+		VGA_COLOR_RED);
+	vga_write_str("                                  KERNEL PANIC!\n");
+
+	vga_clr_from_to(0, 1, VGA_CON_WIDTH, VGA_CON_HEIGHT-1, ' ',
+		VGA_COLOR_RED, VGA_COLOR_BLACK);
+	vga_write_str("\nAn error happened\n");
+
+	vga_clr_from_to(0, VGA_CON_HEIGHT-2, VGA_CON_WIDTH, VGA_CON_HEIGHT,
+		' ', VGA_COLOR_WHITE, VGA_COLOR_RED);
+	vga_write_str("Error code: 0x35\n");
 }
